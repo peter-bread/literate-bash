e28def1f0395033d730c95582cd43e6bdd667d41
Peter Sheehan <petersheehan2003@gmail.com>
2025-08-11 01:10:10 +0100

    feat: start haskell impl

---
+++ Main.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE ViewPatterns #-}
+
+module Main where
+
+import Data.Char (toUpper)
+import Data.List (stripPrefix)
+
+main :: IO ()
+main = do
+  content <- readFile "example.lsh.md"
+  let contentLines = lines content
+  let codeLines = getCodeLines [] False contentLines
+  putStrLn (unlines codeLines)
+
+-- >>> getCodeLines [] False ["hello", "```bash", "foo", "bar", "```", "bye", "```bash", "baz", "```", "end"]
+-- ["foo","bar","baz"]
+getCodeLines :: [String] -> Bool -> [String] -> [String]
+getCodeLines acc _ [] = acc
+getCodeLines acc False (x : xs) = case x of
+  (stripPrefix "```bash" -> Just _) -> getCodeLines acc True xs
+  _ -> getCodeLines acc False xs
+getCodeLines acc True (x : xs) = case x of
+  (stripPrefix "```" -> Just _) -> getCodeLines acc False xs
+  _ -> getCodeLines (acc ++ [x]) True xs
