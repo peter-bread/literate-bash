8d8f81f6724f26d429901e78d4e0b38dd2a6e5fb
Peter Sheehan <petersheehan2003@gmail.com>
2025-08-11 01:53:43 +0100

    refactor(haskell): use clearer types

--- Main.hs
+++ Main.hs
@@ -5,22 +5,27 @@ module Main where
 import Data.Char (toUpper)
 import Data.List (stripPrefix)
 
+type CodeLines = [String]
+
+data ProcessingMode = Code | Normal
+  deriving (Eq, Show)
+
 main :: IO ()
 main = do
   -- TODO: cmdline arg + validation
   content <- readFile "example.lsh.md"
   let contentLines = lines content
-  let codeLines = getCodeLines [] False contentLines
+  let codeLines = getCodeLines [] Normal contentLines
   -- TODO: run the code
   putStrLn (unlines codeLines)
 
--- >>> getCodeLines [] False ["hello", "```bash", "foo", "bar", "```", "bye", "```bash", "baz", "```", "end"]
--- ["foo","bar","baz"]
-getCodeLines :: [String] -> Bool -> [String] -> [String]
-getCodeLines acc _ [] = acc
-getCodeLines acc False (x : xs) = case x of
-  (stripPrefix "```bash" -> Just _) -> getCodeLines acc True xs
-  _ -> getCodeLines acc False xs
-getCodeLines acc True (x : xs) = case x of
-  (stripPrefix "```" -> Just _) -> getCodeLines (acc ++ [""]) False xs
-  _ -> getCodeLines (acc ++ [x]) True xs
+-- >>> getCodeLines [] Normal ["hello", "```bash", "foo", "bar", "```", "bye", "```bash", "baz", "```", "end"]
+-- ["foo","bar","","baz",""]
+getCodeLines :: CodeLines -> ProcessingMode -> [String] -> CodeLines
+getCodeLines acc _ [] = reverse acc
+getCodeLines acc Normal (x : xs) = case x of
+  (stripPrefix "```bash" -> Just _) -> getCodeLines acc Code xs
+  _ -> getCodeLines acc Normal xs
+getCodeLines acc Code (x : xs) = case x of
+  (stripPrefix "```" -> Just _) -> getCodeLines ("" : acc) Normal xs
+  _ -> getCodeLines (x : acc) Code xs
